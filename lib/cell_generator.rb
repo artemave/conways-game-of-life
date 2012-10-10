bd3bb7f7a1d18b78a13a243b2de9d05750b1e2db
artemave <artemave@gmail.com>
2012-10-10 23:08:54 +0100

    Fix bug when dead cell was repopulating with 4 live neighbours

--- lib/cell_generator.rb
+++ lib/cell_generator.rb
@@ -1,10 +1,14 @@
 class CellGenerator
   def self.generate cell_with_neighbours_matrix
-    live_cell_count = cell_with_neighbours_matrix.each.inject(0) do |cnt, cell|
+    target_cell = cell_with_neighbours_matrix[1,1]
+
+    live_neighbours_count = cell_with_neighbours_matrix.each.inject(0) do |cnt, cell|
       cnt += cell.alive? ? 1 : 0
     end
 
-    if live_cell_count >= 3 && live_cell_count <= 4
+    live_neighbours_count -= 1 if target_cell.alive?
+
+    if (target_cell.alive? && live_neighbours_count == 2) || live_neighbours_count == 3
       Cell.new 'live'
     else
       Cell.new 'dead'
--- spec/cell_generator_spec.rb
+++ spec/cell_generator_spec.rb
@@ -4,40 +4,57 @@ describe CellGenerator do
   Given(:live) { Cell.new 'live' }
   Given(:dead) { Cell.new 'dead' }
 
-  context "live cell with 0 live neighbours dies" do
-    Given(:cell_with_neighbours_matrix) { Matrix[[dead,dead,dead],[dead,live,dead],[dead,dead,dead]] }
-    When(:new_cell) { CellGenerator.generate cell_with_neighbours_matrix}
-    Then { new_cell.should_not be_alive }
+  describe "live cell dies" do
+    context "with 0 live neighbours" do
+      Given(:cell_with_neighbours_matrix) { Matrix[[dead,dead,dead],[dead,live,dead],[dead,dead,dead]] }
+      When(:new_cell) { CellGenerator.generate cell_with_neighbours_matrix}
+      Then { new_cell.should_not be_alive }
+    end
+
+    context "with 1 live neighbour" do
+      Given(:cell_with_neighbours_matrix) { Matrix[[dead,dead,dead],[live,live,dead],[dead,dead,dead]] }
+      When(:new_cell) { CellGenerator.generate cell_with_neighbours_matrix}
+      Then { new_cell.should_not be_alive }
+    end
+
+    context "with more than 3 live neighbours" do
+      Given(:cell_with_neighbours_matrix) { Matrix[[live,live,live],[live,live,dead],[dead,dead,dead]] }
+      When(:new_cell) { CellGenerator.generate cell_with_neighbours_matrix}
+      Then { new_cell.should_not be_alive }
+    end
   end
 
-  context "live cell with 1 live neighbours dies" do
-    Given(:cell_with_neighbours_matrix) { Matrix[[dead,dead,dead],[live,live,dead],[dead,dead,dead]] }
-    When(:new_cell) { CellGenerator.generate cell_with_neighbours_matrix}
-    Then { new_cell.should_not be_alive }
+  describe "live cell lives on to the next generation" do
+    context "with 2 live neighbours" do
+      Given(:cell_with_neighbours_matrix) { Matrix[[live,dead,dead],[live,live,dead],[dead,dead,dead]] }
+      When(:new_cell) { CellGenerator.generate cell_with_neighbours_matrix}
+      Then { new_cell.should be_alive }
+    end
+
+    context "with 3 live neighbours" do
+      Given(:cell_with_neighbours_matrix) { Matrix[[live,live,dead],[live,live,dead],[dead,dead,dead]] }
+      When(:new_cell) { CellGenerator.generate cell_with_neighbours_matrix}
+      Then { new_cell.should be_alive }
+    end
   end
 
-  context "live cell with 2 live neighbours lives on to the next generation" do
-    Given(:cell_with_neighbours_matrix) { Matrix[[live,dead,dead],[live,live,dead],[dead,dead,dead]] }
-    When(:new_cell) { CellGenerator.generate cell_with_neighbours_matrix}
-    Then { new_cell.should be_alive }
-  end
-
-  context "live cell with 3 live neighbours lives on to the next generation" do
-    Given(:cell_with_neighbours_matrix) { Matrix[[live,live,dead],[live,live,dead],[dead,dead,dead]] }
-    When(:new_cell) { CellGenerator.generate cell_with_neighbours_matrix}
-    Then { new_cell.should be_alive }
-  end
-
-  context "live cell with more than three live neighbours dies" do
-    Given(:cell_with_neighbours_matrix) { Matrix[[live,live,live],[live,live,dead],[dead,dead,dead]] }
-    When(:new_cell) { CellGenerator.generate cell_with_neighbours_matrix}
-    Then { new_cell.should_not be_alive }
-  end
-
-  context "dead cell with exactly three live neighbours becomes live" do
+  context "dead cell with exactly 3 live neighbours becomes live" do
     Given(:cell_with_neighbours_matrix) { Matrix[[live,live,live],[dead,dead,dead],[dead,dead,dead]] }
     When(:new_cell) { CellGenerator.generate cell_with_neighbours_matrix}
     Then { new_cell.should be_alive }
   end
 
+  describe "dead cell stays dead" do
+    context "when more than 3 live neighbours" do
+      Given(:cell_with_neighbours_matrix) { Matrix[[live,live,live],[live,dead,dead],[dead,dead,dead]] }
+      When(:new_cell) { CellGenerator.generate cell_with_neighbours_matrix}
+      Then { new_cell.should_not be_alive }
+    end
+
+    context "when less than 3 live neighbours" do
+      Given(:cell_with_neighbours_matrix) { Matrix[[live,dead,dead],[live,dead,dead],[dead,dead,dead]] }
+      When(:new_cell) { CellGenerator.generate cell_with_neighbours_matrix}
+      Then { new_cell.should_not be_alive }
+    end
+  end
 end
